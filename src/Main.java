d75184b349122b2271989ee142149969079cf1eb
Emilly <110494320+Emillyzinha@users.noreply.github.com>
2023-06-29 14:28:00 -0300

    DELIVERY

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,32 @@
-// PRIVATE = só acessa na própria classe
-// PACKAGE(DEFAULT) = acessa as classes do pacote
-// PORTECD = tem que ter uma relação de herança para acessar
-// PUBLIC = public
+import BackEnd.Aplicativo;
+import BackEnd.Lanche;
+import BackEnd.Pedidos;
+import BackEnd.Restaurante;
+import BackEnd.Usuario;
+
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Usuario usuario = new Usuario();
-        usuario.setIdade(18);
-        usuario.setNome("Emilly");
-        System.out.println(usuario);
+    public static void main(String[] args) throws IOException {
+        Aplicativo aplicativo = new Aplicativo();
+
+        Restaurante restaurante = new Restaurante("Teste", "Teste");
+
+        restaurante.adicionarLancheCardapio(new Lanche("XTudo", 12.30));
+        restaurante.adicionarLancheCardapio(new Lanche("XNada", 15.00));
+        restaurante.adicionarLancheCardapio(new Lanche("XSalada", 19.60));
+
+//        while (true){
+        System.out.println(restaurante.imprimirCardapio());
+//
+//            System.out.println(restaurante.removerLanche());
+//
+//            System.out.println(restaurante.imprimirCardapio());
+//        }
+
+        Pedidos pedidos = new Pedidos(new Restaurante("Teste", "Teste"), new Usuario("Teste", 12, "oi"));
+
+        System.out.println(pedidos.fazerPedido());
+
     }
 }
